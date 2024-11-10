346067a93a945d72b51843ac3694a0478edd89d5
Yehor <20585619+satelllte@users.noreply.github.com>
2024-11-10 13:25:45 +0200

    noise

--- apps/examples/src/main.ts
+++ apps/examples/src/main.ts
@@ -3,6 +3,15 @@ import { Example00Default } from "./examples/Example00Default";
 import { Example01Color } from "./examples/Example01Color";
 import { Example02Uniforms } from "./examples/Example02Uniforms";
 import { Example03Texture } from "./examples/Example03Texture";
+import { Example04Noise } from "./examples/Example04Noise";
+
+const examples = [
+  Example00Default,
+  Example01Color,
+  Example02Uniforms,
+  Example03Texture,
+  Example04Noise,
+];
 
 const main = async () => {
   if (!WGSLCanvas.isSupported()) {
@@ -13,12 +22,6 @@ const main = async () => {
   const container = document.getElementById("examples");
   if (!container) return;
 
-  const examples = [
-    Example00Default,
-    Example01Color,
-    Example02Uniforms,
-    Example03Texture,
-  ];
   for (const example of examples) {
     const canvas = document.createElement("canvas");
     canvas.width = 250;
