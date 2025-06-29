ce3ce0c633db86e5dcaf30080ab3f283d66cb5a6
Yehor <20585619+satelllte@users.noreply.github.com>
2025-06-30 00:09:48 +0300

    colors

--- apps/docs/src/components/MDX.tsx
+++ apps/docs/src/components/MDX.tsx
@@ -3,13 +3,13 @@
 type H1Props = React.ComponentProps<'h1'>;
 type MDXH1Props = Omit<H1Props, 'className'>;
 function MDXH1(props: MDXH1Props) {
-  return <h1 {...props} className='text-3xl text-gray-50'/>
+  return <h1 {...props} className='text-3xl text-gray12'/>
 }
 
 type H2Props = React.ComponentProps<'h2'>;
 type MDXH2Props = Omit<H2Props, 'className'>;
 function MDXH2(props: MDXH2Props) {
-  return <h2 {...props} className='text-2xl text-gray-50'/>
+  return <h2 {...props} className='text-2xl text-gray12'/>
 }
 
 export const components = {
