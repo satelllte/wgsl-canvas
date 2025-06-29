3ca0c526b8ca2262702d811d67d96dc415eff53f
Yehor <20585619+satelllte@users.noreply.github.com>
2025-06-29 22:29:26 +0300

    mdx demo

--- apps/docs/src/components/Hello.tsx
+++ apps/docs/src/components/Hello.tsx
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react"
+
 export function Hello() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCount(c => c + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    }
+  }, [])
+
   return (
-    <div className="bg-amber-900">Hello</div>
+    <div className="bg-amber-900">Hello {count}</div>
   )
 }
