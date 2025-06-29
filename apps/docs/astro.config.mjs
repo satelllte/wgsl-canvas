b404d850de9dd26c117c56380fae4bcdfa0c8865
Yehor <20585619+satelllte@users.noreply.github.com>
2025-06-29 21:42:10 +0300

    apps/docs new project

---
+++ apps/docs/astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import {defineConfig} from 'astro/config';
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react()],
+  vite: {
+    plugins: [
+      // This most probably started to happen due to some Vite v6 / v7 discrepancies.
+      // Once it's patched properly, the type error should be gone.
+      // @ts-expect-error Type 'HotUpdatePluginContext' is missing the following properties from type 'MinimalPluginContext': debug, error, info, meta, warn
+      tailwindcss()
+    ],
+  },
+});
---
+++ apps/docs/src/components/Hello.tsx
@@ -0,0 +1,5 @@
+export function Hello() {
+  return (
+    <div className="bg-amber-900">Hello</div>
+  )
+}
