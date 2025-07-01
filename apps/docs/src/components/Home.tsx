c5d05157e04aa9fc31db476c7305451b0590cae4
Yehor <20585619+satelllte@users.noreply.github.com>
2025-07-01 15:54:35 +0300

    uniforms section + small UI fixes (img, code)

--- apps/docs/src/components/Home.tsx
+++ apps/docs/src/components/Home.tsx
@@ -6,6 +6,7 @@ export function Home() {
       <InstallationTS/>
       <GetStarted/>
       <PassingShader/>
+      <PassingUniforms/>
     </div>
   )
 }
@@ -74,7 +75,7 @@ await wgslCanvas.init();
 // 4. Make your first render
 wgslCanvas.render();`}</Code>
       <P>If everything is set up correctly, you should see this output (which is the default fragment shader located under <CodeInline>WGSLCanvas.SHADER_FRAGMENT_DEFAULT</CodeInline> static field).</P>
-      <img src="/assets/uv.png" alt="Default fragment shader" width={500} height={500}/>
+      <img src="/assets/uv.png" alt="Default fragment shader" className="max-w-[500px] w-full" width={500} height={500}/>
     </SectionStack>
   )
 }
@@ -98,6 +99,30 @@ fn fragment_main() -> FragmentOutput {
 \`;`}</Code>
       <P>Then pass it to <CodeInline>WGSLCanvas</CodeInline> instance via <CodeInline>shaderFragment</CodeInline> property.</P>
       <Code>{`wgslCanvas.shaderFragment = shaderFragment;`}</Code>
+      <Note type="tip">If you want to store your WGSL code under <CodeInline>.wgsl</CodeInline> files, you should configure your bundler to be able to resolve them as strings. The easiest way is to start with <Link href="https://vite.dev/">Vite</Link>, which can do this out of the box using <Link href="https://vite.dev/guide/assets#importing-asset-as-string"><CodeInline>?raw</CodeInline> suffix</Link>.</Note>
+    </SectionStack>
+  )
+}
+
+function PassingUniforms() {
+  return (
+    <SectionStack>
+      <H2>Passing uniforms</H2>
+      <P>To pass uniforms to your shader, you should first define them in <CodeInline>uniformsKeys</CodeInline> array in your <CodeInline>WGSLCanvas</CodeInline> instance.</P>
+      <Code>{`wgslCanvas.uniformsKeys = ["time", "color1", "color2"];`}</Code>
+      <P>Then, you can pass the values into <CodeInline>uniforms</CodeInline> object.</P>
+      <Code>{`wgslCanvas.uniforms.time = 0.0;
+wgslCanvas.uniforms.color1 = [1.0, 0.0, 0.0];
+wgslCanvas.uniforms.color2 = [0.0, 0.0, 1.0];`}</Code>
+      <P>Then, they'll be available under <CodeInline>{`var<uniform>`}</CodeInline> object at <CodeInline>{`@group(0), @binding(0)`}</CodeInline> in your WGSL shader.</P>
+      <Code>{`@group(0) @binding(0) var<uniform> uniforms: Uniforms;
+
+struct Uniforms {
+  time: f32,
+  color1: vec3<f32>,
+  color2: vec3<f32>,
+}`}</Code>
+      <Note type="important">The order of keys in <CodeInline>struct Uniforms</CodeInline> must be the same as defined in <CodeInline>uniformsKeys</CodeInline> array!</Note>
     </SectionStack>
   )
 }
@@ -110,7 +135,7 @@ function SectionStack({children}: {children: React.ReactNode}) {
   )
 }
 
-function H2({children}: {children: string}) {
+function H2({children}: {children: React.ReactNode}) {
   return <h2 className="text-xl font-medium">{children}</h2>
 }
 
@@ -118,7 +143,19 @@ function P({children}: {children: React.ReactNode}) {
   return <p className="text-gray11">{children}</p>
 }
 
-function Link({href, children}: {href: string, children: string}) {
+function Note({type, children}: {type: 'tip' | 'important', children: React.ReactNode}) {
+  let mark = 'Tip';
+  if (type === 'important') mark = 'Important';
+
+  return <div className="pl-4 flex flex-col gap-1 border-l-2 border-gray05">
+    <div className="text-gray11 flex items-center gap-1"><IconInfo/> <span className="font-medium">{mark}</span></div>
+    <P>
+      {children}
+    </P>
+  </div>
+}
+
+function Link({href, children}: {href: string, children: React.ReactNode}) {
   return <a href={href}
     className="underline decoration-[1.5px] decoration-gray10 text-gray11 hover:decoration-gray12 hover:text-gray12 motion-safe:transition-colors"
     target="_blank"
@@ -126,9 +163,9 @@ function Link({href, children}: {href: string, children: string}) {
   >{children}</a>
 }
 
-function Code({ children }: {children: string}) {
+function Code({ children }: {children: React.ReactNode}) {
   return (
-    <code className="text-gray11 text-sm px-3 py-2 bg-gray02 rounded-md w-full flex min-h-10 items-center border border-gray03 border-solid">
+    <code className="text-gray11 text-sm px-3 py-2 bg-gray02 rounded-md w-full flex min-h-10 items-center border border-gray03 border-solid overflow-x-auto">
       <pre>
         {children}
       </pre>
@@ -136,8 +173,21 @@ function Code({ children }: {children: string}) {
   );
 }
 
-function CodeInline({ children }: { children: string }) {
+function CodeInline({ children }: { children: React.ReactNode }) {
   return (
     <code className="text-gray11 text-sm rounded-md border border-gray03 border-solid bg-gray02 py-0.5 px-1">{children}</code>
+  )
+}
+
+function IconInfo() {
+  return (
+    <svg width="16" height="16" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M7.49991 0.876892C3.84222 0.876892 0.877075 3.84204 0.877075 7.49972C0.877075 11.1574 3.84222 14.1226 7.49991 14.1226C11.1576 14.1226 14.1227 11.1574 14.1227 7.49972C14.1227 3.84204 11.1576 0.876892 7.49991 0.876892ZM1.82707 7.49972C1.82707 4.36671 4.36689 1.82689 7.49991 1.82689C10.6329 1.82689 13.1727 4.36671 13.1727 7.49972C13.1727 10.6327 10.6329 13.1726 7.49991 13.1726C4.36689 13.1726 1.82707 10.6327 1.82707 7.49972ZM8.24992 4.49999C8.24992 4.9142 7.91413 5.24999 7.49992 5.24999C7.08571 5.24999 6.74992 4.9142 6.74992 4.49999C6.74992 4.08577 7.08571 3.74999 7.49992 3.74999C7.91413 3.74999 8.24992 4.08577 8.24992 4.49999ZM6.00003 5.99999H6.50003H7.50003C7.77618 5.99999 8.00003 6.22384 8.00003 6.49999V9.99999H8.50003H9.00003V11H8.50003H7.50003H6.50003H6.00003V9.99999H6.50003H7.00003V6.99999H6.50003H6.00003V5.99999Z"
+        fill="currentColor"
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+      />
+    </svg>
   )
 }
