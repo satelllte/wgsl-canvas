2c9fbf43752a62e1092865648c17a99428e966bb
Yehor <20585619+satelllte@users.noreply.github.com>
2025-07-01 20:21:01 +0300

    Heading: H2Props type

--- apps/docs/src/components/ui/Heading.tsx
+++ apps/docs/src/components/ui/Heading.tsx
@@ -1,3 +1,7 @@
-export function H2({children}: {children: React.ReactNode}) {
+type H2Props = {
+  children: React.ReactNode;
+}
+
+export function H2({children}: H2Props) {
   return <h2 className="text-gray12 text-xl font-medium">{children}</h2>
 }
