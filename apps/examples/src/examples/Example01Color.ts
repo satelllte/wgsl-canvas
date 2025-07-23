7eccd977f7ec8cbbbceab93bec8ca498623f67d2
Yehor <20585619+satelllte@users.noreply.github.com>
2025-07-23 17:15:26 +0300

    remove apps/examples

--- apps/examples/src/examples/Example01Color.ts
+++
@@ -1,9 +0,0 @@
-import { WGSLCanvas } from "@wgsl-canvas/core";
-import shaderFragment from "./Example01Color.wgsl?raw";
-
-export const Example01Color = async (canvas: HTMLCanvasElement) => {
-  const wgslCanvas = new WGSLCanvas({ canvas });
-  await wgslCanvas.init();
-  wgslCanvas.shaderFragment = shaderFragment;
-  wgslCanvas.render();
-};
