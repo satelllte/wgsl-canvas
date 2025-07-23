5ec881d090e8a8e2771fc84c8ba2ebeacafd74d3
satelllte <20585619+satelllte@users.noreply.github.com>
2025-07-23 17:04:48 +0300

    docs (#19)

---
+++ apps/docs/src/components/Example.tsx
@@ -0,0 +1,51 @@
+import { LayoutSide } from "./LayoutSide";
+import { Link } from "./ui/Link";
+
+const examples = [
+  'color',
+  'default',
+  'noise',
+  'texture',
+  'uniforms',
+];
+
+type ExampleProps = {
+  name: string;
+}
+
+export function Example({ name }: ExampleProps) {
+  const hrefSource = `https://github.com/satelllte/wgsl-canvas/tree/main/apps/docs/src/pages/examples/${name}.astro`;
+  return (
+    <LayoutSide name="Examples" sidebar={(
+      <div className="flex flex-col items-start">
+        {examples.map(example => {
+          return (
+            <LinkToExample key={example} name={example} selected={example === name}/>
+          );
+        })}
+      </div>
+    )}
+    >
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-0">
+          <h2 className="text-lg font-semibold">{name}</h2>
+          <Link size="sm" href={hrefSource}>View source</Link>
+        </div>
+        <canvas id="canvas" className="max-w-[512px] w-full" width="512" height="512"></canvas>
+      </div>
+    </LayoutSide>
+  )
+}
+
+type LinkToExampleProps = {
+  name: string;
+  selected: boolean;
+};
+
+function LinkToExample({name, selected}: LinkToExampleProps) {
+  return (
+    <Link size="sm" weight={selected ? 'bold' : undefined} href={`/examples/${name}`}>
+      {name}
+    </Link>
+  );
+}
---
+++ apps/docs/src/components/LayoutSide.tsx
@@ -0,0 +1,22 @@
+type LayoutSideProps = {
+  name: string;
+  children: React.ReactNode;
+  sidebar?: React.ReactNode;
+};
+
+export function LayoutSide({name, children, sidebar}: LayoutSideProps) {
+  return (
+    <div className="p-4 flex flex-col md:flex-row gap-8 md:gap-4">
+      <div className="min-w-3xs">
+        <div className="flex flex-col gap-4">
+          <div>
+            <a href="/" className="text-gray12 font-bold text-2xl">WGSL Canvas</a>
+            <h1 className="text-gray-11 text-sm">{name}</h1>
+          </div>
+          {sidebar}
+        </div>
+      </div>
+      {children}
+    </div>
+  )
+}
